64d0288c75f4699f0aee9d5d10605e523d4a2982
Oscar-Cabr <oscar.cabrerarojas@gmail.com>
2025-02-16 11:55:14 -0600

    adding: old submitted but unrated problems

---
+++ Codeforces/800-900/2051A - Preparing for the Olympiad.cpp
@@ -0,0 +1,79 @@
+/*
+    The following is an original solution to the problem: 2051-A "Preparing for the Olympiad" of Codeforces.
+    Rated difficulty: 800. Language: C++20 (GCC 13-64).
+    This solution isn't secured as "the most efficient one", but it is secured as an Accepted one under the given constraints.
+    Techniques / Topics involved: Adhoc, Implementation, Greedy.
+
+    Check out the problem at: https://codeforces.com/contest/2051/problem/A
+
+    The only purpose of doing this is to learn and improve my skills in competitive programming.
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> ii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<ii> vii;
+typedef vector<vi> vvi;
+typedef vector<vii> vvii;
+#define fi first
+#define se second
+#define pb push_back
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define sz(a) (int)(a.size())
+#define fori(i,a,n) for(int i = a; i < n; i++)
+#define endl '\n'
+const int MOD = 1e9+7;
+const int INFTY = INT_MAX;
+const long long LLINF = LLONG_MAX;
+const double EPS = DBL_EPSILON;
+void fastIO() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+}
+void setIO() {
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+}
+
+void solve( int n, vi& a, vi& b )
+{
+    int sol = a[n-1];
+
+    fori(i,0,n-1)
+    {
+        if( a[i] - b[i+1] > 0 )
+            sol += a[i] - b[i+1];
+    }
+
+    cout << sol << endl;
+}
+
+int main() {
+    fastIO();
+
+    int tc, n;
+    
+    cin >> tc;
+    
+    fori(i,0,tc)
+    {
+        cin >> n;
+        vi a(n), b(n);
+        fori(j,0,n)
+            cin >> a[j];
+        fori(j,0,n)
+            cin >> b[j];
+        solve( n, a, b );
+    }
+    
+    return 0;
+}
---
+++ Codeforces/800-900/2053A - Tender Carpenter.cpp
@@ -0,0 +1,83 @@
+/*
+    The following is an original solution to the problem: 2053-A "Tender Carpenter" of Codeforces.
+    Rated difficulty: 800. Language: C++20 (GCC 13-64).
+    This solution isn't secured as "the most efficient one", but it is secured as an Accepted one under the given constraints.
+    Techniques / Topics involved: Greedy, Math.
+
+    Check out the problem at: https://codeforces.com/contest/2053/problem/A
+
+    The only purpose of doing this is to learn and improve my skills in competitive programming.
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> ii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<ii> vii;
+typedef vector<vi> vvi;
+typedef vector<vii> vvii;
+#define fi first
+#define se second
+#define pb push_back
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define sz(a) (int)(a.size())
+#define fori(i,a,n) for(int i = a; i < n; i++)
+#define endl '\n'
+const int MOD = 1e9+7;
+const int INFTY = INT_MAX;
+const long long LLINF = LLONG_MAX;
+const double EPS = DBL_EPSILON;
+void fastIO() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+}
+void setIO() {
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+}
+
+void solve( int n, vi& nums )
+{
+    int sol = 0, maxV, minV;
+
+    fori(i,1,n)
+    {
+        maxV = max( nums[i], nums[i-1] );
+        minV = min( nums[i], nums[i-1] );
+        if( minV * 2 > maxV || minV == maxV )
+        {
+            sol++;
+            break;
+        }
+    }
+
+    cout << ( (sol > 0) ? "YES" : "NO" ) << endl;
+}
+
+int main()
+{
+    fastIO();
+    
+    int tc, n;
+
+    cin >> tc;
+
+    fori(i,0,tc)
+    {
+        cin >> n;
+        vi nums(n);
+        fori(j,0,n)
+            cin >> nums[j];
+        solve(n, nums);
+    }
+    
+    return 0;
+}
